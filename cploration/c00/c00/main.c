408fdd9a3a1f174bab93bfee191762dc2e82a1d0
Cameron839 <cameronpeters839@yahoo.com>
2022-09-27 08:29:47 -0700

    Initial commit.

---
+++ cploration/c00/c00/main.c
@@ -0,0 +1,14 @@
+
+/****************************************
+* C-ploration 0 for CS 271
+*
+* Cameron Peters
+* Fall 2022
+*****************************************/
+#include <stdio.h>
+int main() {
+  // printf(.) displays the string given
+  printf("Hello world!\n");
+  // main returns 0 if exiting without error
+  return 0;
+}
