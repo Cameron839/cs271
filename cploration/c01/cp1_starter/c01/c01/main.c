ac223258f5a20252e8fb787e4fc36245ee1aa50e
Cameron839 <cameronpeters839@yahoo.com>
2022-10-05 21:17:32 -0700

    implemented Nand

---
+++ cploration/c01/cp1_starter/c01/c01/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  c01
+//
+//  Created by Cameron Peters  on 10/1/22.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
